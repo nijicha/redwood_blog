4b2e1586f6b28d3e1f0dfe4d750c742437c39585
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2022-04-26 03:19:40 +0700

    Add faker.js for .mock.ts files

--- web/src/components/ArticlesCell/ArticlesCell.mock.ts
+++ web/src/components/ArticlesCell/ArticlesCell.mock.ts
@@ -1,4 +1,24 @@
-// Define your own mock data here:
+import { faker } from '@faker-js/faker'
+
 export const standard = (/* vars, { ctx, req } */) => ({
-  articles: [{ id: 42 }, { id: 43 }, { id: 44 }],
+  articles: [
+    {
+      id: faker.unique(faker.datatype.number),
+      title: faker.lorem.text(),
+      body: faker.lorem.sentences(5),
+      createdAt: faker.datatype.datetime(),
+    },
+    {
+      id: faker.unique(faker.datatype.number),
+      title: faker.lorem.text(),
+      body: faker.lorem.sentences(5),
+      createdAt: faker.datatype.datetime(),
+    },
+    {
+      id: faker.unique(faker.datatype.number),
+      title: faker.lorem.text(),
+      body: faker.lorem.sentences(5),
+      createdAt: faker.datatype.datetime(),
+    },
+  ],
 })
--- web/src/components/ArticlesCell/ArticlesCell.test.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from '@redwoodjs/testing/web'
-import { Loading, Empty, Failure, Success } from './ArticlesCell'
+import { render } from '@redwoodjs/testing/web'
+import { Empty, Failure, Loading, Success } from './ArticlesCell'
 import { standard } from './ArticlesCell.mock'
 
 // Generated boilerplate tests do not account for all circumstances
